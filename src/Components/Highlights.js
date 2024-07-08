7c557d302dbf0e1bf9b66c69b466c240f2b5db08
Brian Whitaker <43477669+bwwhitaker@users.noreply.github.com>
2024-07-08 10:27:53 -0700

    Fix Order Online Button Links

--- src/Components/Highlights.js
+++ src/Components/Highlights.js
@@ -8,21 +8,21 @@ export default function Highlights() {
 			image:
 				'https://images.unsplash.com/photo-1505253716362-afaea1d3d1af?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3648&q=80',
 			title: 'Greek Salad',
-			price: '$12.99',
+			price: '$11.50',
 			description: 'Crisp lettuce, peppers, olives, and feta cheese garnished with crunchy rosemary croutons.',
 		},
 		{
 			image:
 				'https://images.unsplash.com/photo-1506280754576-f6fa8a873550?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=4000&q=80',
 			title: 'Bruchetta',
-			price: '$5.99',
+			price: '$7.00',
 			description: 'Grilled bread smeared with garlic and seasoned with salt and olive oil.',
 		},
 		{
 			image:
 				'https://images.unsplash.com/photo-1560180474-e8563fd75bab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2000&q=80',
 			title: 'Lemon Tart',
-			price: '$5.00',
+			price: '$4.99',
 			description: "Straight from grandma's recipe book, every ingredient is locally grown.",
 		},
 	];
